df8e46494713d72d84883f5db9fd9c47b8e85c7a
rioishii <rioishii@uw.edu>
2019-01-17 10:41:14 -0800

    check point

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,44 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+    func add(_ nums: [Int]) -> Int {
+        var sum = 0
+        for n in nums {
+            sum += n
+        }
+        return sum
+    }
+    
+    func multiply(_ nums: [Int]) -> Int {
+        switch nums.count {
+        case _ where nums.count == 0 :
+            return 0
+        default :
+            var sum = 1
+            for n in nums {
+                sum *= n
+            }
+            return sum
+        }
+    }
+    
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
 }
